ab5d6b98d65dddbb2cb94438c4398aed5621b6de
Pablo Valencia <pablolgn24@gmail.com>
2018-06-20 11:20:59 +0200

    hh

---
+++ resources/layers/page357097365-sketchedArial.js
@@ -0,0 +1,163 @@
+rabbit.data.layerStore.addLayerFromHtml('<div xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:sketchedHelper="java:it.rapidrabb.editor.stencils.helpers.SketchedHelper" xmlns:fn="http://www.w3.org/2005/xpath-functions">\
+   <div id="result">\
+      <div xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns="http://www.w3.org/1999/xhtml" id="__containerId__page357097365-layer" class="layer" name="__containerId__pageLayer" data-layer-id="page357097365" data-layer-type="pageLayer" style="position:absolute;left:0px;top:0px;">\
+         <div id="__containerId__-page357097365-layer-47240231" style="position: absolute; left: 98px; top: 285px; width: 48px; height: 48px" data-interactive-element-type="fonticon.icon" class="icon pidoco-clickable-element stencil mobile-interaction-potential-trigger " data-stencil-id="47240231" data-review-reference-id="47240231">\
+            <div class="stencil-wrapper" style="width: 48px; height: 48px">\
+               <div xmlns:pidoco="http://www.pidoco.com/util" style="width:48px;height:48px;" title="">\
+                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-orange">\
+                     <svg style="position: absolute; width: 0; height: 0;" width="0" height="0" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><symbol xmlns:a0="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" id="icon-glyph-e180" preserveAspectRatio="xMidYMid meet">\
+<title>glyph-e180</title>\
+<path class="path1" d="M214.133 729.359v-66.167q0-6.674 4.765-11.597t11.756-5.004h562.696q6.911 0 11.756 5.004t4.765 11.597v66.167q0 6.99-4.765 11.756t-11.756 4.765h-562.696q-6.911 0-11.756-4.765t-4.765-11.756zM215.324 571.848q-2.145-8.659 2.145-14.218l284.922-367.451q4.291-5.242 10.088-4.926t10.089 5.639l284.366 367.371q4.291 5.639 2.304 13.9t-8.659 8.262h-576.515q-6.592 0-8.738-8.578z"/>\
+</symbol><symbol xmlns:a0="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" id="icon-glyph-2709" preserveAspectRatio="xMidYMid meet">\
+<title>glyph-2709</title>\
+<path class="path1" d="M114.842 745.882l231.704-231.622 165.456 165.456 165.456-165.457 231.704 231.622h-794.316zM114.842 679.716v-397.158l198.579 198.579zM114.842 216.392h794.316l-397.158 397.158zM710.579 481.138l198.579-198.579v397.158z"/>\
+</symbol><symbol xmlns:a0="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" id="icon-android-e126" preserveAspectRatio="xMidYMid meet">\
+<title>android-e126</title>\
+<path class="path1" d="M929.021 660.404h-125.41v-51.749h73.795v-413.586h-730.807v413.586h85.146v51.749h-136.762v-517.084h834.038v517.084zM674.504 501.596v-63.365c0-86.335-70.23-156.564-156.431-156.564s-156.564 70.23-156.564 156.564v63.365h-70.757v251.084h454.642v-251.084h-70.889zM421.97 438.232c0-53.069 43.167-96.235 96.101-96.235 53.069 0 96.101 43.167 96.101 96.235v63.365h-192.206v-63.365z"/>\
+</symbol><symbol xmlns:a0="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" id="icon-android-e089" preserveAspectRatio="xMidYMid meet">\
+<title>android-e089</title>\
+<path class="path1" d="M960.705 735.25c10.031-13.201 9.241-31.946-2.772-43.959-1.451-1.451-3.036-2.639-4.619-3.695v-0.265l-195.639-139.93-0.265 0.131c-13.069-9.637-31.549-8.582-43.432 3.301-1.19 1.19-2.112 2.509-3.036 3.829-0.661 0.791-1.319 1.585-1.716 2.509-13.201 18.747-13.069 21.121-23.628 37.492-15.181 23.628-32.475 60.062-49.768 101.515-54.913-8.449-136.762-56.235-226.266-145.739-89.636-89.503-137.29-171.349-145.739-226.266 41.452-17.293 77.887-34.586 101.647-49.635 16.236-10.693 18.613-10.562 37.492-23.628 0.791-0.53 1.585-1.055 2.374-1.716 0 0 0.131 0 0.131 0 1.19-0.924 2.639-1.981 3.695-3.168 11.881-11.749 12.805-30.231 3.301-43.432v-0.131l-139.799-195.771h-0.265c-1.19-1.585-2.374-3.036-3.695-4.49-12.012-12.012-30.891-12.805-43.959-2.772-93.727 64.157-178.607 134.782-167.388 218.212 11.881 89.636 118.677 286.99 258.343 426.656s337.155 246.461 426.656 258.212c83.562 11.222 154.056-73.53 218.346-167.256z"/>\
+</symbol><symbol xmlns:a0="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" id="icon-glyph-e231" preserveAspectRatio="xMidYMid meet">\
+<title>glyph-e231</title>\
+<path class="path1" d="M148.282 489.080q-1.668-53.299 10.724-104.611t36.46-95.158 59.176-81.259 77.684-64.021 93.094-42.336 104.453-17.396q13.9-0.635 40.349 0.635 9.612 0.397 11.439 4.527t-4.765 8.42q-76.175 49.009-119.704 129.237t-40.192 173.24q2.304 70.535 31.932 133.605t77.605 107.867 113.35 70.137 136.226 23.035q29.152-1.351 48.691-5.322 11.201-2.304 13.027 0.874t-5.085 10.723q-52.982 58.62-123.833 91.665t-153.54 35.745q-53.616 1.668-105.246-10.724t-95.636-36.221-81.577-58.939-64.419-77.286-42.656-92.697-17.554-103.737z"/>\
+</symbol><symbol xmlns:a0="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" id="icon-glyph-e012" preserveAspectRatio="xMidYMid meet">\
+<title>glyph-e012</title>\
+<path class="path1" d="M81.719 699.573v-337.585q0-46.308 33.123-79.433t79.433-33.124h112.873q0.318-0.953 0.953-3.652l10.645-25.815q11.597-28.755 41.701-49.247t61.242-20.572h180.708q31.057 0 61.163 20.572t41.702 49.247l10.645 25.815q0.635 2.701 0.953 3.652h112.873q46.308 0 79.433 33.124t33.124 79.433v337.585q0 46.308-33.124 79.433t-79.433 33.123h-635.454q-46.308 0-79.433-33.123t-33.124-79.433zM338.284 547.303q0 47.025 23.273 87.057t63.385 63.385 87.057 23.353q35.425 0 67.516-13.741t55.443-37.095 37.095-55.602 13.662-67.357-13.662-67.357-37.095-55.602-55.443-37.015-67.516-13.741q-47.025 0-87.057 23.353t-63.385 63.385-23.353 86.978zM420.972 547.303q0-37.73 26.688-64.34t64.34-26.609 64.34 26.609 26.689 64.34-26.689 64.419-64.34 26.609-64.34-26.609-26.688-64.419zM776.745 381.847h66.246v-33.123h-66.246v33.123z"/>\
+</symbol><symbol xmlns:a0="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" id="icon-glyph-e222" preserveAspectRatio="xMidYMid meet">\
+<title>glyph-e222</title>\
+<path class="path1" d="M139.306 480.976q0-5.799 5.322-9.77l354.105-261.17q5.322-3.972 9.294-1.986t3.972 8.659v149.57q15.569 1.351 32.567 3.652t44.719 8.42 53.617 14.773 56.795 23.512 56.872 33.759 51.472 46.468 43.369 60.369 29.311 77.128 11.915 95q-11.597-27.801-30.66-50.28t-41.861-37.413-49.962-26.688-54.808-17.634-56.237-10.487-54.969-5.085-50.28-1.509-42.495 0.16v165.457q0 6.99-3.972 8.975t-9.294-2.304l-353.471-261.807q-5.322-3.972-5.322-9.77z"/>\
+</symbol><symbol xmlns:a0="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" id="icon-glyph-e223" preserveAspectRatio="xMidYMid meet">\
+<title>glyph-e223</title>\
+<path class="path1" d="M131.364 729.359q0.715-50.599 11.915-95t29.31-77.128 43.369-60.369 51.472-46.546 56.953-33.759 56.715-23.433 53.616-14.773 44.719-8.42 32.567-3.652v-149.57q0-6.674 3.972-8.659t9.295 1.987l354.105 261.17q5.322 3.972 5.322 9.77t-5.322 9.77l-353.471 261.807q-5.639 4.291-9.453 2.304t-4.131-8.975v-165.457q-18.19-1.033-42.336-0.16t-50.28 1.509-54.808 5.085-56.237 10.406-54.808 17.713-49.962 26.689-41.861 37.413-30.66 50.28z"/>\
+</symbol><symbol xmlns:a0="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" id="icon-glyph-e245" preserveAspectRatio="xMidYMid meet">\
+<title>glyph-e245</title>\
+<path class="path1" d="M116.51 487.412q0-63.228 50.599-113.984t132.414-73.316q-10.564 31.139-10.564 61.878 0 51.312 26.292 97.463t71.17 79.272 107.232 52.425 131.857 19.382q1.668 0 5.004-0.16t4.607-0.16q-40.669 36.38-100.641 57.588t-128.362 21.208q-60.925 0-117.163-17.554l-115.493 60.528q-5.639 3.337-8.659 1.033t-1.27-8.578l27.801-101.99q-35.425-27.482-55.126-62.037t-19.699-72.997zM349.165 351.106q0-54.013 38.207-99.686t103.977-72.282 143.453-26.689 143.452 26.689 103.897 72.282 38.287 99.686q0 37.73-19.382 71.965t-54.411 61.399l27.404 100.56q1.668 5.958-1.113 8.34t-8.42-0.715l-114.222-59.573q-56.557 16.918-115.493 16.918-77.764 0-143.452-26.689t-103.977-72.443-38.207-99.766z"/>\
+</symbol><symbol xmlns:a0="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" id="icon-glyph-e241" preserveAspectRatio="xMidYMid meet">\
+<title>glyph-e241</title>\
+<path class="path1" d="M114.842 481.138q0-80.782 31.455-154.257t84.755-126.773 126.693-84.674 154.257-31.455q51.948 0 102.467 13.741t93.332 38.047 79.433 58.383 62.672 73.871 41.384 85.389 16.918 92.3l0.318 2.304h83.404q9.214 0 9.214 6.674 0 3.575-2.621 6.592l-129.075 188.65q-3.972 5.639-9.93 5.639t-9.93-5.639l-129.075-188.65q-2.621-2.939-2.621-6.592 0-6.674 9.214-6.674h81.815q-2.383-52.585-27.008-101.434t-64.18-84.516-93.491-57.27-112.237-21.526q-60.527 0-115.651 23.669t-95 63.546-63.545 95-23.669 115.652q0 44.005 11.121 83.562t29.39 69.026 42.893 54.094 50.836 41.066 53.617 27.801 50.916 16.521 43.211 5.163q64.895 0 110.251-14.457t87.694-47.42q19.143-15.251 41.701-35.745 5.639-5.322 14.535-5.322 14.535 0 26.134 12.55l22.56 24.148q10.883 11.597 10.883 25.181 0 9.612-6.275 15.886-0.635 0.635-5.799 5.639t-12.709 11.597-20.255 16.521q-25.815 19.539-51.234 34.235t-58.939 28.119-74.347 20.573-87.534 7.070q-28.436 0-61.719-7.548t-69.821-23.829-71.648-39.081-66.882-56.078-55.761-72.362-38.048-89.996-14.059-106.915zM379.586 630.071v-165.457q0-6.99 4.846-11.756t11.756-4.846h16.521v-50.28q0-34.395 23.035-58.224t56.078-23.83h40.349q33.124 0 56.078 23.83t23.035 58.224v50.28h16.521q6.99 0 11.756 4.846t4.846 11.756v165.457q0 6.99-4.846 11.756t-11.756 4.765h-231.622q-6.99 0-11.756-4.765t-4.846-11.756zM478.876 448.014h66.246v-50.28q0-7.309-3.496-11.597t-9.453-4.291h-40.349q-5.958 0-9.453 4.291t-3.496 11.597v50.28z"/>\
+</symbol></defs></svg>\
+                     <!--load fonticon glyph-e222-->\
+                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-glyph-e222"></use>\
+                  </svg>\
+               </div>\
+            </div>\
+            <div class="interactive-stencil-highlighter" style="width: 48px; height: 48px"></div><script xmlns:json="http://json.org/" type="text/javascript">\
+			$(document).ready(function(){\
+				rabbit.interaction.manager.registerInteraction(\'__containerId__-page357097365-layer-47240231\', \'interaction545900443\', {"button":"left","id":"action253868018","numberOfFinger":"1","type":"click"},  \
+					[\
+						{"delay":"100","id":"reaction368029149","options":"withoutReloadOnly","target":"page674100365","transition":"none","type":"showPage"}\
+					]\
+				);\
+			});\
+		</script></div>\
+         <div id="__containerId__-page357097365-layer-1502936809" style="position: absolute; left: 486px; top: 285px; width: 48px; height: 48px" data-interactive-element-type="fonticon.icon" class="icon pidoco-clickable-element stencil mobile-interaction-potential-trigger " data-stencil-id="1502936809" data-review-reference-id="1502936809">\
+            <div class="stencil-wrapper" style="width: 48px; height: 48px">\
+               <div xmlns:pidoco="http://www.pidoco.com/util" style="width:48px;height:48px;" title="">\
+                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-orange">\
+                     <!--print symbols here-->\
+                     <!--load fonticon glyph-e223-->\
+                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-glyph-e223"></use>\
+                  </svg>\
+               </div>\
+            </div>\
+            <div class="interactive-stencil-highlighter" style="width: 48px; height: 48px"></div><script xmlns:json="http://json.org/" type="text/javascript">\
+			$(document).ready(function(){\
+				rabbit.interaction.manager.registerInteraction(\'__containerId__-page357097365-layer-1502936809\', \'interaction769578763\', {"button":"left","id":"action67006274","numberOfFinger":"1","type":"click"},  \
+					[\
+						{"delay":"100","id":"reaction713010604","options":"withoutReloadOnly","target":"page265389653","transition":"none","type":"showPage"}\
+					]\
+				);\
+			});\
+		</script></div>\
+         <div id="__containerId__-page357097365-layer-59211342" style="position: absolute; left: 305px; top: 260px; width: 24px; height: 24px" data-interactive-element-type="fonticon.icon" class="icon stencil mobile-interaction-potential-trigger " data-stencil-id="59211342" data-review-reference-id="59211342">\
+            <div class="stencil-wrapper" style="width: 24px; height: 24px">\
+               <div xmlns:pidoco="http://www.pidoco.com/util" style="width:24px;height:24px;" title="">\
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="fill-orange">\
+                     <!--print symbols here-->\
+                     <!--load fonticon glyph-e180-->\
+                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-glyph-e180"></use>\
+                  </svg>\
+               </div>\
+            </div>\
+         </div>\
+         <div id="__containerId__-page357097365-layer-1062672960" style="position: absolute; left: 180px; top: 290px; width: 48px; height: 48px" data-interactive-element-type="fonticon.icon" class="icon stencil mobile-interaction-potential-trigger " data-stencil-id="1062672960" data-review-reference-id="1062672960">\
+            <div class="stencil-wrapper" style="width: 48px; height: 48px">\
+               <div xmlns:pidoco="http://www.pidoco.com/util" style="width:48px;height:48px;" title="">\
+                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-white">\
+                     <!--print symbols here-->\
+                     <!--load fonticon glyph-e012-->\
+                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-glyph-e012"></use>\
+                  </svg>\
+               </div>\
+            </div>\
+         </div>\
+         <div id="__containerId__-page357097365-layer-1990345864" style="position: absolute; left: 295px; top: 210px; width: 48px; height: 48px" data-interactive-element-type="fonticon.icon" class="icon stencil mobile-interaction-potential-trigger " data-stencil-id="1990345864" data-review-reference-id="1990345864">\
+            <div class="stencil-wrapper" style="width: 48px; height: 48px">\
+               <div xmlns:pidoco="http://www.pidoco.com/util" style="width:48px;height:48px;" title="">\
+                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-white">\
+                     <!--print symbols here-->\
+                     <!--load fonticon android-e126-->\
+                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-android-e126"></use>\
+                  </svg>\
+               </div>\
+            </div>\
+         </div>\
+         <div id="__containerId__-page357097365-layer-icon806471971" style="position: absolute; left: 410px; top: 290px; width: 48px; height: 48px" data-interactive-element-type="fonticon.icon" class="icon stencil mobile-interaction-potential-trigger " data-stencil-id="icon806471971" data-review-reference-id="icon806471971">\
+            <div class="stencil-wrapper" style="width: 48px; height: 48px">\
+               <div xmlns:pidoco="http://www.pidoco.com/util" style="width:48px;height:48px;" title="">\
+                  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="fill-white">\
+                     <!--print symbols here-->\
+                     <!--load fonticon glyph-e231-->\
+                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-glyph-e231"></use>\
+                  </svg>\
+               </div>\
+            </div>\
+         </div>\
+      </div>\
+   </div>\
+   <div id="styles">\
+      <style type="text/css">\
+         			.repository[data-review-reference-id="page357097365"], .transition-wrapper[data-page-id="page357097365"] .layer-container{\
+         				background-image: url(\'../repoimages/683776.jpg\');\
+         				background-size: contain;\
+         				background-repeat: no-repeat;\
+         			}\
+         		\
+         	\
+         		body[data-current-page-id="page357097365"] .border-wrapper, body[data-current-page-id="page357097365"] .simulation-container{\
+         			width:640px;\
+         		}\
+         		\
+         		body.has-frame[data-current-page-id="page357097365"] .border-wrapper, body.has-frame[data-current-page-id="page357097365"]\
+         .simulation-container{\
+         			height:360px;\
+         		}\
+         		\
+         		body[data-current-page-id="page357097365"] .svg-border-640-360{\
+         			display: block !important;\
+         		}\
+         		\
+         		body[data-current-page-id="page357097365"] .border-wrapper .border-div{\
+         			width:640px;\
+         			height:360px;\
+         		}\
+         	\
+      </style>\
+   </div>\
+   <div id="json">\
+      		{\
+      			"id": "page357097365",\
+      			"name": "menu 4",\
+      			"layers": {\
+      				\
+      			},\
+      			"image":"../repoimages/683776.jpg",\
+      			"width":640,\
+      			"height":360,\
+      			"parentFolder": "",\
+      			"frame": "smartphone",\
+      			"frameOrientation": "landscape"\
+      		}\
+      	\
+   </div>\
+</div>');
---
+++ resources/layers/page39074354-sketchedArial.js
@@ -0,0 +1,60 @@
+rabbit.data.layerStore.addLayerFromHtml('<div xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:sketchedHelper="java:it.rapidrabb.editor.stencils.helpers.SketchedHelper" xmlns:fn="http://www.w3.org/2005/xpath-functions">\
+   <div id="result">\
+      <div xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns="http://www.w3.org/1999/xhtml" id="__containerId__page39074354-layer" class="layer" name="__containerId__pageLayer" data-layer-id="page39074354" data-layer-type="pageLayer" style="position:absolute;left:0px;top:0px;"><script xmlns:json="http://json.org/" type="text/javascript">\
+			$(document).ready(function(){\
+				rabbit.interaction.manager.registerInteraction(\'__containerId__page39074354-layer\', \'interaction769570234\', {"id":"action4649800","type":"onPageLoad"},  \
+					[\
+						{"delay":"0","duration":"1000","id":"reaction526159132","type":"vibrate"}\
+								,\
+							{"delay":"200","id":"reaction941912712","options":"withoutReloadOnly","target":"page753642822","transition":"opacity","type":"showPage"}\
+					]\
+				);\
+			});\
+		</script></div>\
+   </div>\
+   <div id="styles">\
+      <style type="text/css">\
+         			.repository[data-review-reference-id="page39074354"], .transition-wrapper[data-page-id="page39074354"] .layer-container{\
+         				background-image: url(\'../repoimages/683776.jpg\');\
+         				background-size: contain;\
+         				background-repeat: no-repeat;\
+         			}\
+         		\
+         	\
+         		body[data-current-page-id="page39074354"] .border-wrapper, body[data-current-page-id="page39074354"] .simulation-container{\
+         			width:640px;\
+         		}\
+         		\
+         		body.has-frame[data-current-page-id="page39074354"] .border-wrapper, body.has-frame[data-current-page-id="page39074354"]\
+         .simulation-container{\
+         			height:360px;\
+         		}\
+         		\
+         		body[data-current-page-id="page39074354"] .svg-border-640-360{\
+         			display: block !important;\
+         		}\
+         		\
+         		body[data-current-page-id="page39074354"] .border-wrapper .border-div{\
+         			width:640px;\
+         			height:360px;\
+         		}\
+         	\
+      </style>\
+   </div>\
+   <div id="json">\
+      		{\
+      			"id": "page39074354",\
+      			"name": "Inicio",\
+      			"layers": {\
+      				\
+      			},\
+      			"image":"../repoimages/683776.jpg",\
+      			"width":640,\
+      			"height":360,\
+      			"parentFolder": "",\
+      			"frame": "smartphone",\
+      			"frameOrientation": "landscape"\
+      		}\
+      	\
+   </div>\
+</div>');
